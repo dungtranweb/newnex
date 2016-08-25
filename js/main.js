1cd6fee1ff22f1853696de40956d91fa193ab9cb
Dung Tran <dungtranweb@gmail.com>
2016-08-25 16:46:54 +0700

    Setup

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// LOAD FULLPAGE.JS
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#ccddff'],
+      anchors: ['home', 'about', 'news', 'contact'],
+      menu: '#menu'
+    });
+});
+
+// LOAD WOW.JS
+$(document).ready(function() {
+  new WOW().init();
+})
+
+// LOAD GOOGLE MAP
+
+
+//
