3271a6dc34fc525440995afde6de97fa9bd5ea7f
Dung Tran <dungtranweb@gmail.com>
2016-08-26 16:36:28 +0700

    Desktop view has done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,78 @@
+// LOAD GOOGLE MAP
+// window.onload = function () {
+//   "use strict";
+//
+// };
+
 // LOAD FULLPAGE.JS
 $(document).ready(function() {
     $('#fullpage').fullpage({
       anchors: ['home', 'about', 'news', 'contact'],
-      menu: '#menu'
+      menu: '#menu',
+      afterRender: function () {
+
+        // LOAD GOOGLE MAP
+        var styles = [
+        {
+          featureType: 'water', // set the water color
+          elementType: 'geometry.fill', // apply the color only to the fill
+          stylers: [
+            { color: '#FFFFFF' }
+          ]
+        },{
+          featureType: 'landscape.manmade', // set the natural landscape
+          elementType: 'all',
+          stylers: [
+            { hue: '#27354D' }
+          ]
+        },{
+          featureType: 'poi', // set the point of interest
+          elementType: 'all',
+          stylers: [
+            { hue: '#0F1E39' }
+          ]
+        },{
+          featureType: 'road', // set the road
+          elementType: 'geometry',
+          stylers: [
+            { hue: '#0F1E39' }
+          ]
+        },{
+          featureType: 'road.local', // set the local road
+          elementType: 'all',
+          stylers: [
+            { hue: '#0F1E39' }
+          ]
+        }
+      ];
+
+      var myLatlng = new google.maps.LatLng(10.794519, 106.702320);
+
+      var options = {
+        mapTypeControlOptions: {
+          mapTypeIds: ['Nexx']
+        },
+        center: myLatlng,
+        zoom: 17,
+        disableDefaultUI: false,
+        scrollwheel: false,
+        mapTypeId: 'Nexx'
+
+      };
+
+      var div = document.getElementById('location');
+      var map = new google.maps.Map(div, options);
+      var styledMapType = new google.maps.StyledMapType(styles, { name: 'Nexx' });
+      map.mapTypes.set('Nexx', styledMapType);
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'NEX'
+        });
+        
+      }
+
     });
 });
 
@@ -11,7 +81,20 @@ $(document).ready(function() {
   new WOW().init();
 })
 
-// LOAD GOOGLE MAP
+// LOAD OWL CAROUSEL
+$(document).ready(function(){
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      items:5,
+      center: true,
+      nav: true,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:5000,
+      autoplayHoverPause:true
+      });
+});
 
 
 // TYPEWRITER EFFECT
